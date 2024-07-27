9dd2d27d9010ce209e7884016af18c96fc0840d6
leonardo Krindges <krindgesleonardo3@gmail.com>
2024-07-27 15:13:10 -0300

    feature/ Adicionado theme padrão

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { theme } from "./config/theme/DefaultTheme";
 
 export function App() {
-
   return (
-    <>
-      <h1>Bora implementar esse front</h1>
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <h1>Sistema de transferências</h1>
+    </ThemeProvider>
+  );
 }
